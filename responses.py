746ea32f1724da874cb55f950aab9bc4682e50d6
CrimsonRobin <CrimsonRobin807@outlook.com>
2023-12-27 23:19:07 -0500

    Update responses.py

--- responses.py
+++ responses.py
@@ -4,6 +4,6 @@ import re
 def handle_response(message):
     p_message = message.lower()     # not really necessary, but I'll keep it if I want to add more commands
 
-    l_message = re.split(r"[, \-!?:#]+", p_message)
-    # print(l_message) - for debugging
+    l_message = re.split(r"[,\s\-!?:#]+", p_message)
+    # print(l_message)   # for debugging
     return "https://scp-wiki.wikidot.com/scp-" + l_message[-1]
